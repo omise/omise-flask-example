40ce2cadcf6cdb8061ddb02de26b79e387e0967a
Daniel Fowler <daniel@omise.co>
2019-09-02 18:14:28 +0700

    Retrieve charge status from webhook

--- webhook/webhook.py
+++ webhook/webhook.py
@@ -8,4 +8,7 @@ webhook = Blueprint("webhook", __name__)
 def order():
     event = request.json
     current_app.logger.info(f"Event: {event['key']}")
+    if event["data"]["object"] == "charge":
+        charge_status = event["data"]["status"]
+        current_app.logger.info(f"Event: {charge_status}")
     return redirect("/")
